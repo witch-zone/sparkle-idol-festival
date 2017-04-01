b7ea5f44b5760b6aa658e6be0f99798f5076e6b6
dani quinn sexton <dani@goneupinflames.com>
2017-04-01 20:43:34 +0100

    Wait for otonokizaka to load before showing the app! 🏫 ⌚️

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import Promyfill from 'promise-polyfill'
 
 import SparkleIdolFestival from './app'
 
+import otonokizaka from 'assets/images/otonokizaka.jpg'
+import logo from 'assets/images/ribbon-logo.png'
+
 require('./sass/preload.scss')
 require('./sass/sif.scss')
+
+if (!window.Promise) {
+  window.Promise = Promyfill
+}
+
+const promiseMeAnImage = src => new Promise(
+  (resolve, reject) => {
+    const img = new Image()
+    img.onload = resolve
+    img.onerror = reject
+    img.src = src
+  }
+)
 
 render(
   <BrowserRouter>
@@ -15,13 +32,14 @@ render(
   () => {
     const showApp = () => setTimeout(
       () => {
-      document.getElementById("💭").classList.add("‼️")
-      document.getElementById('✨').classList.add("‼️")
-      }, 666
+        document.getElementById("💭").classList.add("‼️")
+        document.getElementById('✨').classList.add("‼️")
+      }, 250
     )
 
-    const logo = document.getElementById("🌠")
-    logo.onload = showApp
-    logo.src = logo.src
+    Promise.all([
+      promiseMeAnImage(otonokizaka),
+      promiseMeAnImage(logo),
+    ]).then(showApp)
   }
 )
