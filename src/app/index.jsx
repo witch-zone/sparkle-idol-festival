aa1b6a76c0adaae4b14076e83d7c798f8e27f59a
dani quinn sexton <dani@goneupinflames.com>
2017-04-02 17:30:13 +0100

    Conditionally set transition name based on route location!! ➡️ ⭐️

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -6,24 +6,29 @@ import { Routes as IdolRoutes } from './components/idols'
 import ScoutingResults from './components/ScoutingResults'
 
 const SparkleIdolFestival = () => (
-  <Route render={({ location }) => (
-    <Transition
-      component="main"
-      className="o-wrapper"
-      transitionName="t-page-move"
-      transitionEnterTimeout={600}
-      transitionLeaveTimeout={600}
-    >
-      <Switch
-        location={location}
-        key={location.key}
+  <Route render={({ location }) => {
+    const isIdolRoute = !!location.pathname.match(/\/idols/)
+    const transitionName = isIdolRoute ? "t-idol-move" : "t-page-move"
+
+    return (
+      <Transition
+        component="main"
+        className="o-wrapper"
+        transitionName={transitionName}
+        transitionEnterTimeout={600}
+        transitionLeaveTimeout={600}
       >
-        <Route path="/idols" component={IdolRoutes} />
-        <Route exact path="/" component={ScoutingResults} />
-        <Redirect to="/" />
-      </Switch>
-    </Transition>
-  )} />
+        <Switch
+          location={location}
+          key={location.key}
+        >
+          <Route path="/idols" component={IdolRoutes} />
+          <Route exact path="/" component={ScoutingResults} />
+          <Redirect to="/" />
+        </Switch>
+      </Transition>
+    )
+  }} />
 )
 
 export default SparkleIdolFestival
